dfe20ea976b970e8ac748abae392d185b9dc8dd4
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-04-10 18:47:45 +0930

    added variables

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -1,3 +1,4 @@
+//Variables for moment.js current time
 var dateNow = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var hourNow = moment().format('h:mm:ss a');
 
@@ -7,4 +8,18 @@ var interval = setInterval(function() {
                         + momentNow.format('dddd')
                          .substring(0,3).toUpperCase());
     $('#dateNow').html(dateNow + " " + momentNow.format('HH:mm:ss A'));
-  }, 100);
+}, 100);
+
+//Variables for time
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
+var eightPm = $("#20pm");
